1aac3b6c1c6486d6ee38f3acb06987c29d4cd8cc
Juan Gómez <80915299+xoan04@users.noreply.github.com>
2022-04-04 11:44:45 -0500

    Dispatch

--- Ejercicio_Polimorf.py
+++ Ejercicio_Polimorf.py
@@ -15,3 +15,18 @@ def llama_hablar(x):
 llama_hablar(Entrenador())
 llama_hablar(Futbolista())
 llama_hablar(Masajista())
+
+
+#USO DE DISPATCH
+from multipledispatch import dispatch
+@dispatch(int, int) #Add que funciona como suma de enteros
+def add(a, b):
+    return print(a + b)
+@dispatch(str, str)
+def add(a, b):  #Add que funciona como concatenacion de cadenas
+    return print(" ".join((a, b)))
+
+
+
+add("Hola","mundo")
+add(5,6)
